57a7f05a86bb763a6ad0664bfdf4cfe35c29a72a
IWSTTY <107331336+IWSTTY@users.noreply.github.com>
2022-06-12 18:39:26 +0900

    PDFのテキスト抽出

---
+++ 雑多なもの/PDFのテキスト抽出.py
@@ -0,0 +1,20 @@
+from pdfminer.pdfinterp import PDFResourceManager,PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+
+input_path = "03-yoshi.pdf"
+output_path="result.txt"
+
+rsrcmgr=PDFResourceManager()
+codec="utf-8"
+params=LAParams()
+
+with open(output_path,'ab') as output:
+    device=TextConverter(rsrcmgr,output,codec=codec,laparams=params)
+    with open(input_path,"rb") as input:
+        interpreter=PDFPageInterpreter(rsrcmgr,device)
+        for page in PDFPage.get_pages(input):
+            interpreter.process_page(page)
+
+    device.close()
